5142d16e631637dcc65e3772112c64e4dfc97a52
Kenny Tran <khtran@ucsb.edu>
2023-09-13 21:32:06 -0700

    add draw window

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ LINE_WIDTH = 5
 screen = pg.display.set_mode(WINDOW_SIZE)
 pg.display.set_caption("MNIST Drawing Test Thing")
 
+# Drawing area (x, y, width, height)
+drawing_area = pg.Rect(50, 50, 400, 400)
+
+drawing_surface = pg.Surface(drawing_area.size)
+drawing_surface.fill(WHITE)
+
 # Init variables
 is_drawing = False
 last_pos = None
@@ -25,22 +31,30 @@ screen.fill(WHITE)
 
 while is_active:
     for event in pg.event.get():
+        # Handle Quit
         if event.type == pg.QUIT:
             active = False
+        # Handle Drawing
         elif event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
-            is_drawing = True
-            last_pos = event.pos
+            # Only draw if within borders
+            if drawing_area.collidepoint(event.pos):
+                is_drawing = True
+                last_pos = event.pos
         elif event.type == pg.MOUSEBUTTONUP and event.button == 1:
             is_drawing = False
             last_pos = None
         elif event.type == pg.MOUSEMOTION:
             if is_drawing:
-                if last_pos != None:
+                if last_pos != None and drawing_area.collidepoint(event.pos):
                     pg.draw.line(screen, DRAW_COLOR, last_pos, event.pos, LINE_WIDTH)
                 last_pos = event.pos
+        # Handle Erasing
         elif event.type == pg.MOUSEBUTTONUP and event.button == 3:
             screen.fill(WHITE)
     
+    # Draw the area border
+    pg.draw.rect(screen, BLACK, drawing_area, 2)
+
     # Update display
     pg.display.flip()
 
